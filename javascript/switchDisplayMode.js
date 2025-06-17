84b43cb2c5ebba8e393103afefdca8efd89c810a
JK82NL <jk-dance@hotmail.com>
2025-06-17 11:27:29 +0200

    Removed unused fullscreenOverlay from html, css and switchDisplay.

--- javascript/switchDisplayMode.js
+++ javascript/switchDisplayMode.js
@@ -5,35 +5,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const monthPage = document.getElementById('monthPage');
     const weekPage = document.getElementById('weekPage');
     const dayPage = document.getElementById('dayPage');
-    const fullscreenOverlay = document.getElementById('fullscreenOverlay');
 
     // Set initial states
     appointmentPage.style.display = "none";
     weekPage.style.display = "none";
     dayPage.style.display = "none";
-    fullscreenOverlay.style.display = "none";
     monthPage.style.display = "block"; // Show month page by default
 });
 
 // Switch page with buttonclick.
 function switchPage(targetPage) {
-    // DEBUG LOG
-    console.log('switchPage called with:', targetPage);
 
-    // DEBUG LOG to check if elements are found
-    console.log('Elements found:', {
-        appointmentPage,
-        monthPage,
-        weekPage,
-        dayPage,
-        fullscreenOverlay
-    });
-
-    // Put page variables into an array and them all.
-    const pages = [appointmentPage, monthPage, weekPage, dayPage, fullscreenOverlay];
+    // Put page variables into an array and hide them all.
+    const pages = [appointmentPage, monthPage, weekPage, dayPage];
     pages.forEach(page => page.style.display = "none");
 
-    // Show the target page based on button clicked
+    // Show the page based on the button which was clicked.
     switch (targetPage) {
         case 'appointmentPage':
             appointmentPage.style.display = "block";
@@ -46,9 +33,6 @@ function switchPage(targetPage) {
             break;
         case 'dayPage':
             dayPage.style.display = "block";
-            break;
-        case 'fullscreenOverlay':
-            fullscreenOverlay.style.display = "block";
             break;
         default:
             monthPage.style.display = "block"; // Default to month view
