45cf7954c41809d025931280d92ef4ff21c50687
JK82NL <jk-dance@hotmail.com>
2025-06-24 15:13:45 +0200

    Unfinished in between commit.

---
+++ javascript/appointmentOnCalendar.js
@@ -0,0 +1,29 @@
+const appointmentCheck = document.querySelectorAll(".dot-indicator");
+
+console.log(appointmentCheck);
+
+const appointmentIndicator = appointmentCheck.closest(".parent");
+
+console.log(appointmentIndicator);
+
+const appointmentElement = appointmentIndicator.closest(".parent");
+
+console.log(appointmentElement);
+
+function appointmentPopup() {
+
+}
+
+//     if (appointmentCheck === dot-indicator)
+
+
+//         class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Popover title" data-bs-content="And here’s some amazing content. It’s very engaging. Right?"
+// }
+
+// document.getElementsByClassName("flatpickr-day")
+
+// div id0 class = appointment - card
+// strong = title
+// small = description
+
+// let appointmentsThisDay = document.querySelectorAll("");
